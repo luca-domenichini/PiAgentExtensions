47485d0ed65c1c1f51aec19d9d54ca889a6d07ad
Luca Domenichini <luca.domenic@gmail.com>
2026-07-29 17:23:06 +0200

    + model name on console

--- model-cost-display.ts
+++ model-cost-display.ts
@@ -301,7 +301,7 @@ export default function (pi: ExtensionAPI) {
 
 		// Notification 2: cost + AA score
 		setTimeout(() => {
-			const costLine = `💰 Cost/1M tok: Input ${fmtCost(cost.input)} | Output ${fmtCost(cost.output)} | CacheRead ${fmtCost(cost.cacheRead)}`;
+			const costLine = `🧠 ${model.id} 💰 Cost/1M tok: Input ${fmtCost(cost.input)} | Output ${fmtCost(cost.output)} | CacheRead ${fmtCost(cost.cacheRead)}`;
 			ctx.ui.notify(`${costLine} | AA Intelligence: ${aaStr}${fromFile}`, "info");
 		}, 300);
 
