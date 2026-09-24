287bacf20cb9bcf4597f9ac6fb25851be4c9fd33
Luca Domenichini <luca.domenic@gmail.com>
2026-09-24 10:20:09 +0200

    - deleted obsolete current-folder.ts

--- current-folder.ts
+++
@@ -1,9 +0,0 @@
-/** Display the current working directory in the footer. */
-
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-
-export default function (pi: ExtensionAPI) {
-	pi.on("session_start", async (_event, ctx) => {
-		ctx.ui.setStatus("current-folder", "💻 " + ctx.cwd + "\\>");
-	});
-}
